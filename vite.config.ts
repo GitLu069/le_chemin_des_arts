79d835decd66f6042ae5b8d15c8cc86aa98ef795
GitLu069 <lucasvincent69250@gmail.com>
2025-05-12 15:23:00 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/le_chemin_des_arts/',  // Ajout de la base du projet
+  base: '/',  // Ajout de la base du projet
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
