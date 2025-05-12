f282f6cde84454a7c0673d750360f619394ddfc2
GitLu069 <lucasvincent69250@gmail.com>
2025-05-12 16:37:26 +0200

    Fix homepage package4

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Utilisation de BrowserRouter
 
 import Index from "./pages/Index";
 import Explore from "./pages/Explore";
@@ -16,31 +12,23 @@ import Infos from "./pages/Infos";
 import About from "./pages/About";
 import Location from "./pages/Location";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter> {/* Remplacement de BrowserRouter par HashRouter */}
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/explore" element={<Explore />} />
-          <Route path="/merci" element={<ThankYou />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/legal" element={<Legal />} />
-          <Route path="/concours" element={<Concours />} />
-          <Route path="/infos" element={<Infos />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/location/:slug" element={<Location />} />
-          <Route path="/oeuvre-:id" element={<Artwork />} />
-          <Route path="/:slug" element={<Artwork />} />
-          <Route path="*" element={<NotFound />} /> {/* Remplacement de Index par NotFound pour les 404 */}
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <BrowserRouter> {/* Utilisation de BrowserRouter */}
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/explore" element={<Explore />} />
+      <Route path="/merci" element={<ThankYou />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/legal" element={<Legal />} />
+      <Route path="/concours" element={<Concours />} />
+      <Route path="/infos" element={<Infos />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/location/:slug" element={<Location />} />
+      <Route path="/oeuvre-:id" element={<Artwork />} />
+      <Route path="/:slug" element={<Artwork />} />
+      <Route path="*" element={<NotFound />} /> {/* Remplacement de Index par NotFound pour les 404 */}
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
