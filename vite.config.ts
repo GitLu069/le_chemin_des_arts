3156385e86a1b86064731e84fdc135166d742a91
GitLu069 <lucasvincent69250@gmail.com>
2025-05-12 14:54:01 +0200

    Update base config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/le_chemin_des_arts/', // Remplacez par le nom exact de votre dépôt
+  base: '/le_chemin_des_arts/',  // Ajout de la base du projet
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
